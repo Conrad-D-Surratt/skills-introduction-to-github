1b9ac8da9a41a452193090f39fd2325f1cd9e767
Conrad Surratt <csurratt376@students.mchenry.edu>
2026-01-26 22:02:25 -0600

    Add files via upload

---
+++ The RPG Character Profile.py
@@ -0,0 +1,27 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+# Define different variables
+print()
+item_name = "Sprint Shoes"   # String
+quantity = 1               # Integer
+price = 99.99               # Float
+on_sale = False              # Boolean
+shop_name = "Try N' Save" #Extra String
+
+# Print variables using f-strings
+print(f"Item name: {item_name}")
+print(f"Quantity: {quantity}")
+print(f"Price: {price}")
+print(f"On sale: {on_sale}")
+print(f"Shop Name: {shop_name}\n")
